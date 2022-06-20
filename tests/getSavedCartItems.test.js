47a13c1204018051a794fbcf41bc01f1cd76a3f1
Solon <amaral.solon@hotmail.com>
2022-06-20 14:55:46 -0300

    Requisito 7 :checkered_flag:

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,18 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('checks if localStorage.getItem is called in getSavedCartItems', () => {
+    expect.assertions(1);
+
+    getSavedCartItems();
+
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it('checks if localStorage.getItem is called when cartItems is given as argument', () => {
+    expect.assertions(1);
+
+    getSavedCartItems();
+
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });

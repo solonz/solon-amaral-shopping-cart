7fa219bfae4e5efc0431a2e14351827dcd502b21
Solon <amaral.solon@hotmail.com>
2022-06-20 14:51:42 -0300

    Requisito 6 :checkered_flag:

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,14 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('checks if localStorage.setItem is called when "<ol><li>Item</li></ol>" is given as argument', () => {
+    expect.assertions(1);
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled()
+  });
+  it('checks if localStorage.setItem is called with two parameters when "<ol><li>Item</li></ol>" is given as argument', () => {
+    expect.assertions(1);
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+});
 });
